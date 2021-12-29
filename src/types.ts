fb2d88f730b7d51f82ed261398173c2df0d37a87
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-29 19:04:11 +0300

    feat: создать файл с интерфейсами цветов

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface MenuColorsI {
+  background: string;
+  sectionText: string;
+  marker: string;
+  sectionIcon: string;
+}
+
+export interface BaseColorsI {
+  titleAndText: string;
+  tags: string;
+  icons: string;
+  workspace: string;
+}
+
+export interface NotificationColorsI {
+  success: string;
+  danger: string;
+  warning: string;
+  info: string;
+}
