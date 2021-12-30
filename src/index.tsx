c716fc6c8f049ce02bc5fffe96693cdac1ad2ff7
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-30 17:43:59 +0300

    style: подключить шрифты

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 
+import "fonts/SourceSansPro/SourceSansPro-Regular.ttf";
+import "fonts/SourceSansPro/SourceSansPro-SemiBold.ttf";
+
 import "styles/main.css";
 import "styles/normalize.css";
 
