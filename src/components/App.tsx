9f314d554a3d9b1ef0249d2c8f5ddb2eb514b976
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-30 13:27:12 +0300

    feat: добавить типы к стейту и коллбеку в App компоненте

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
 
-import { MenuColorsI, BaseColorsI, NotificationColorsI } from "types";
+import {
+  MenuColorsI,
+  BaseColorsI,
+  NotificationColorsI,
+  OnColorChangeType,
+} from "types";
 
 function App() {
-  const [menuColors, setMenuColors] = useState({
+  const [menuColors, setMenuColors] = useState<MenuColorsI>({
     background: "#2B3D4F",
     sectionText: "#FFFFFF",
     marker: "#5286CA",
     sectionIcon: "#9CB2C9",
   });
 
-  const [baseColors, setBaseColors] = useState({
+  const [baseColors, setBaseColors] = useState<BaseColorsI>({
     titleAndText: "#333333",
     tags: "#8A91A7",
     icons: "#587AB0",
     workspace: "#FFFFFF",
   });
 
-  const [notificationColors, setNotificationColors] = useState({
-    success: "#449637",
-    danger: "#C15652",
-    warning: "#F6C75A",
-    info: "#5659CE",
-  });
+  const [notificationColors, setNotificationColors] =
+    useState<NotificationColorsI>({
+      success: "#449637",
+      danger: "#C15652",
+      warning: "#F6C75A",
+      info: "#5659CE",
+    });
 
-  const onColorsChange = (
-    menuColors: MenuColorsI,
-    baseColors: BaseColorsI,
-    notificationColors: NotificationColorsI
+  const onColorChange: OnColorChangeType = (
+    menuColors,
+    baseColors,
+    notificationColors
   ) => {
     setMenuColors(menuColors);
     setBaseColors(baseColors);
