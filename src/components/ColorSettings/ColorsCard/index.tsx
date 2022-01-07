82fc5e0d9ef967ab9dd5f19d0eb4ed1bee0a3b43
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2022-01-07 15:50:55 +0300

    refactor: заменить содержимое ColorsCard на CardHeader и CardBody компоненты

--- src/components/ColorSettings/ColorsCard/index.tsx
+++ src/components/ColorSettings/ColorsCard/index.tsx
@@ -1,66 +1,27 @@
 import React from "react";
 
-import { MenuColorsI, BaseColorsI, NotificationColorsI } from "types";
+import { ColorGroupI } from "types";
 
-import { ReactComponent as CheckIcon } from "icons/check.svg";
-import { ReactComponent as CrossIcon } from "icons/cross.svg";
-
-import ColorItem from "components/ColorSettings/ColorsCard/ColorItem";
+import CardHeader from "components/ColorSettings/ColorsCard/CardHeader";
+import CardBody from "components/ColorSettings/ColorsCard/CardBody";
 
 interface ColorsCardProps {
-  menuColors: MenuColorsI;
-  baseColors: BaseColorsI;
-  notificationColors: NotificationColorsI;
+  title: string;
+  description?: string;
+  colorGroups: ColorGroupI[];
   onClose: React.MouseEventHandler<HTMLDivElement>;
 }
 
 function ColorsCard({
-  menuColors,
-  baseColors,
-  notificationColors,
+  title,
+  description,
+  colorGroups,
   onClose,
 }: ColorsCardProps) {
   return (
     <div className="colors-card">
-      <div className="colors-card__header">
-        <div className="title colors-card__title">Стили оформления</div>
-        <div className="colors-card__icons">
-          <div className="colors-card__icon">
-            <CheckIcon />
-          </div>
-          <div className="colors-card__icon" onClick={onClose}>
-            <CrossIcon />
-          </div>
-        </div>
-      </div>
-      <div className="text colors-card__description">
-        Здесь вы можете настроить цвета сайта
-      </div>
-      <div className="colors-card__divider"></div>
-      <div className="title colors-card__list-title">ОСНОВНЫЕ ЦВЕТА</div>
-      <ColorItem
-        {...{
-          colors: Object.values(menuColors) as string[],
-          title: "Оформление меню",
-          description: "Настройте цвета меню",
-        }}
-      />
-      <ColorItem
-        {...{
-          colors: Object.values(baseColors) as string[],
-          title: "Базовые элементы",
-          description: "Цвета крупных элементов интерфейса",
-        }}
-      />
-      <div className="colors-card__divider"></div>
-      <div className="title colors-card__list-title">ДОПОЛНИТЕЛЬНЫЕ ЦВЕТА</div>
-      <ColorItem
-        {...{
-          colors: Object.values(notificationColors) as string[],
-          title: "Уведомления",
-          description: "Цвета всплывающих полсказок",
-        }}
-      />
+      <CardHeader {...{ title, onClose }} />
+      <CardBody {...{ description, colorGroups }} />
     </div>
   );
 }
