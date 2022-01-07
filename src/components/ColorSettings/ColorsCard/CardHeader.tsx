1880a883fdbd34864bb48c299555d786ac3d7824
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2022-01-07 15:48:45 +0300

    feat: создать ColorsCardHeader компонент

---
+++ src/components/ColorSettings/ColorsCard/CardHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { ReactComponent as CheckIcon } from "icons/check.svg";
+import { ReactComponent as CrossIcon } from "icons/cross.svg";
+
+interface CardHeaderProps {
+  title: string;
+  onClose: React.MouseEventHandler<HTMLDivElement>;
+}
+
+function CardHeader({ title, onClose }: CardHeaderProps) {
+  return (
+    <div className="colors-card__header">
+      <div className="title colors-card__title">{title}</div>
+      <div className="colors-card__icons">
+        <div className="colors-card__icon">
+          <CheckIcon />
+        </div>
+        <div className="colors-card__icon" onClick={onClose}>
+          <CrossIcon />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardHeader;
