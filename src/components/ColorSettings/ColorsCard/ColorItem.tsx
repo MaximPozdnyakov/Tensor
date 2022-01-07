677d31ac4a02dcaef0a54b88b2ca7e67dd3256d5
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2022-01-07 14:20:56 +0300

    feat: создать ColorItem компонент

---
+++ src/components/ColorSettings/ColorsCard/ColorItem.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { ReactComponent as ArrowIcon } from "icons/arrow.svg";
+import { ReactComponent as SliderIcon } from "icons/slider.svg";
+
+interface ColorItemProps {
+  colors: string[];
+  title: string;
+  description: string;
+}
+
+function ColorItem({ colors, title, description }: ColorItemProps) {
+  const icon = colors.length === 1 ? <SliderIcon /> : <ArrowIcon />;
+  const iconClassName = colors.length === 1 ? "color-item__icon" : "";
+
+  const thumbnailBg = colors.length === 1 ? colors[0] : undefined;
+  const thumbnailParts =
+    colors.length === 4 &&
+    [colors[0], colors[1], colors[3], colors[2]].map((color, i) => (
+      <div
+        className="color-item__thumbnail-part"
+        key={`${color}-${i}`}
+        style={{ background: color }}
+      ></div>
+    ));
+
+  return (
+    <div className="colors-card__item color-item">
+      <div
+        className="color-item__thumbnail"
+        style={{ background: thumbnailBg }}
+      >
+        {thumbnailParts}
+      </div>
+      <div className="color-item__text-wrapper">
+        <div className="title color-item__title">{title}</div>
+        <div className="text">{description}</div>
+      </div>
+      <div className={iconClassName}>{icon}</div>
+    </div>
+  );
+}
+
+export default ColorItem;
