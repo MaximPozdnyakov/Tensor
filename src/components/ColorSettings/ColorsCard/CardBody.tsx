83a5058346ecaefee7604e37eee12a88cb9353c3
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2022-01-07 15:48:25 +0300

    feat: создать ColorsCardBody компонент

---
+++ src/components/ColorSettings/ColorsCard/CardBody.tsx
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react";
+
+import { ColorGroupI } from "types";
+
+import ColorItem from "components/ColorSettings/ColorsCard/ColorItem";
+
+interface CardBodyProps {
+  description?: string;
+  colorGroups: ColorGroupI[];
+}
+
+function CardBody({ description, colorGroups }: CardBodyProps) {
+  return (
+    <>
+      {description && (
+        <div className="text colors-card__description">{description}</div>
+      )}
+      {colorGroups.map(({ title, items }) => (
+        <Fragment key={`group-${title}`}>
+          {colorGroups.length > 1 && (
+            <div className="colors-card__divider"></div>
+          )}
+
+          {title && (
+            <div className="title colors-card__list-title">{title}</div>
+          )}
+
+          {items.map(({ colors, title, description }) => (
+            <ColorItem {...{ colors, title, description }} />
+          ))}
+        </Fragment>
+      ))}
+    </>
+  );
+}
+
+export default CardBody;
